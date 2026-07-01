8e7cc82d0159818c2b21afaa26f2b33e28456e6c
epoha35-cpu <epoha35@gmail.com>
2026-07-01 23:13:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ async function getUserById(id) {
   const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
   return result.rows[0];
 }
+function renderMessages(chat) {
+    console.log('🔄 ОТОБРАЖЕНИЕ СООБЩЕНИЙ:', chat);
+    console.log('📨 Сообщения:', chat.messages);
+}
 
 async function getChats(userId) {
   const result = await pool.query(
